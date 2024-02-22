3368dc967035af93b74b95467ac6b3de0a483638
I2L1A1 <101645426+I2L1A1@users.noreply.github.com>
2024-02-22 21:27:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let tg = window.Telegram.WebApp;
-let user_id = tg.initDataUnsafe.user.id;
+let user_id = tg.initDataUnsafe.user.name;
 
 tg.expand();
 
@@ -76,5 +76,6 @@ btn_add_6.addEventListener("click", () => {
 })
 
 Telegram.WebApp.onEvent("mainButtonClicked", () => {
-    tg.sendData("user_id=" + user_id + ", item_id=" + item_id);
+    let x = "user_id=" + user_id + ", item_id=" + item_id;
+    tg.sendData(x);
 });
