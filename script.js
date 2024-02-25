d6027b640dbeb546e5d11f367bd28b96f0b25add
I2L1A1 <101645426+I2L1A1@users.noreply.github.com>
2024-02-25 11:08:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ tg.MainButton.setText("Оформить заказ");
 tg.MainButton.show();
 
 class ItemFromCatalog {
-    constructor(item_img, item_name) {
+    constructor(item_img, item_name, item_cost) {
         this.item_img = item_img;
         this.item_name = item_name;
+        this.item_cost = item_cost + " ₽";
     }
 }
 
@@ -17,8 +18,8 @@ class Catalog {
     Items = new Array(1);
     size = 0;
 
-    addItem(item_name, item_img) {
-        this.Items.push(new ItemFromCatalog(item_name, item_img));
+    addItem(item_name, item_img, item_cost) {
+        this.Items.push(new ItemFromCatalog(item_name, item_img, item_cost));
         this.size += 1;
     }
 }
@@ -26,26 +27,27 @@ class Catalog {
 class Order {
     order_items = new Array(catalog.size + 1).fill(document.createElement("label"));
 
+    order_cost = 0;
+
     generate_data_for_send() {
         let answer_string = "";
         for (let i = 1; i < this.order_items.length; ++i) {
             answer_string += `item_id=${i}, item_counter=${this.order_items[i].textContent}`
-            if (i !== this.order_items.length - 1) {
-                answer_string += ", "
-            }
+            answer_string += ", "
         }
+        answer_string += `order_cost=${this.order_cost}`;
         return answer_string;
     }
 }
 
 catalog = new Catalog();
 
-catalog.addItem("Dish1.png", "Блюдо 1");
-catalog.addItem("Dish1.png", "Блюдо 2");
-catalog.addItem("Dish1.png", "Блюдо 3");
-catalog.addItem("Dish1.png", "Блюдо 4");
-catalog.addItem("Dish1.png", "Блюдо 5");
-catalog.addItem("Dish1.png", "Блюдо 6");
+catalog.addItem("Dish1.png", "Блюдо 1", "100");
+catalog.addItem("Dish1.png", "Блюдо 2", "200");
+catalog.addItem("Dish1.png", "Блюдо 3", "150");
+catalog.addItem("Dish1.png", "Блюдо 4", "290");
+catalog.addItem("Dish1.png", "Блюдо 5", "175");
+catalog.addItem("Dish1.png", "Блюдо 6", "120");
 
 order = new Order();
 
@@ -64,6 +66,10 @@ for (let i = 1; i < catalog.size + 1; ++i) {
     graphicCatalogItems[i].item_name.className = "item_name";
     graphicCatalogItems[i].item_name.textContent = catalog.Items[i].item_name;
 
+    graphicCatalogItems[i].item_cost = document.createElement("div");
+    graphicCatalogItems[i].item_cost.className = "item_cost";
+    graphicCatalogItems[i].item_cost.textContent = catalog.Items[i].item_cost;
+
     graphicCatalogItems[i].item_btn = document.createElement("button");
     graphicCatalogItems[i].item_btn.className = "btn_add";
     graphicCatalogItems[i].item_btn.textContent = "Добавить";
@@ -74,12 +80,14 @@ for (let i = 1; i < catalog.size + 1; ++i) {
     document.getElementById("items").appendChild(graphicCatalogItems[i]);
     graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_img);
     graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_name);
+    graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_cost);
     graphicCatalogItems[i].appendChild(graphicCatalogItems[i].add_remove_figures);
     graphicCatalogItems[i].add_remove_figures.appendChild(graphicCatalogItems[i].item_btn);
 }
 
 for (let i = 1; i < catalog.size + 1; ++i) {
     graphicCatalogItems[i].item_btn.addEventListener("click", () => {
+        order.order_cost += parseInt(graphicCatalogItems[i].item_cost.textContent);
 
         graphicCatalogItems[i].item_btn.style.display = "none";
 
@@ -102,6 +110,7 @@ for (let i = 1; i < catalog.size + 1; ++i) {
         graphicCatalogItems[i].plus_btn.addEventListener("click", () => {
             let new_number = +(order.order_items[i].textContent);
             new_number += 1;
+            order.order_cost += parseInt(graphicCatalogItems[i].item_cost.textContent);
             order.order_items[i].textContent = new_number + "";
             console.log(order.generate_data_for_send());
         });
@@ -118,6 +127,7 @@ for (let i = 1; i < catalog.size + 1; ++i) {
                 order.order_items[i].style.display = "none";
                 order.order_items[i].textContent = "0";
             }
+            order.order_cost -= parseInt(graphicCatalogItems[i].item_cost.textContent);
             console.log(order.generate_data_for_send());
         });
 
