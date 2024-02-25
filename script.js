1cd5b80030357d1b4b4b983c5b40170bbd2bcfc6
I2L1A1 <101645426+I2L1A1@users.noreply.github.com>
2024-02-25 12:49:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,161 +1,120 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
-
-// tg.MainButton.textColor = "#2d2d2d";
-// tg.MainButton.color = "#ffbf74";
-// tg.MainButton.setText("Оформить заказ");
-// tg.MainButton.show();
-
-class ItemFromCatalog {
-    constructor(item_img, item_name, item_cost) {
-        this.item_img = item_img;
-        this.item_name = item_name;
-        this.item_cost = item_cost + " ₽";
-    }
+body {
+    margin: 0;
+    padding: 0;
+    background-color: white;
+    user-select: none;
 }
 
-class Catalog {
-    Items = new Array(1);
-    size = 0;
-
-    addItem(item_name, item_img, item_cost) {
-        this.Items.push(new ItemFromCatalog(item_name, item_img, item_cost));
-        this.size += 1;
-    }
+.container {
+    width: 390px;
 }
 
-class Order {
-    order_items = new Array(catalog.size + 1).fill(document.createElement("label"));
-
-    order_cost = 0;
-
-    generate_data_for_send() {
-        let answer_string = "";
-        for (let i = 1; i < this.order_items.length; ++i) {
-            answer_string += `item_id=${i}, item_counter=${this.order_items[i].textContent}`
-            answer_string += ", "
-        }
-        answer_string += `order_cost=${this.order_cost}`;
-        return answer_string;
-    }
+.items {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
 }
 
-catalog = new Catalog();
-
-catalog.addItem("Dish1.png", "Блюдо 1", "100");
-catalog.addItem("Dish1.png", "Блюдо 2", "200");
-catalog.addItem("Dish1.png", "Блюдо 3", "150");
-catalog.addItem("Dish1.png", "Блюдо 4", "290");
-catalog.addItem("Dish1.png", "Блюдо 5", "175");
-catalog.addItem("Dish1.png", "Блюдо 6", "45");
-catalog.addItem("Dish1.png", "Блюдо 7", "777");
-catalog.addItem("Dish1.png", "Блюдо 8", "23");
-catalog.addItem("Dish1.png", "Блюдо 9", "67");
-catalog.addItem("Dish1.png", "Блюдо 10", "89");
-catalog.addItem("Dish1.png", "Блюдо 11", "1000");
-catalog.addItem("Dish1.png", "Блюдо 12", "2000");
-catalog.addItem("Dish1.png", "Блюдо 13", "1030");
-catalog.addItem("Dish1.png", "Блюдо 14", "333");
-catalog.addItem("Dish1.png", "Блюдо 15", "777");
-catalog.addItem("Dish1.png", "Блюдо 16", "120");
-
-order = new Order();
-
-let graphicCatalogItems = new Array(catalog.size + 1);
-
-for (let i = 1; i < catalog.size + 1; ++i) {
-    graphicCatalogItems[i] = document.createElement("div");
-    graphicCatalogItems[i].className = "item";
-
-    graphicCatalogItems[i].item_img = document.createElement("img");
-    graphicCatalogItems[i].item_img.src = catalog.Items[i].item_img;
-    graphicCatalogItems[i].item_img.alt = "";
-    graphicCatalogItems[i].item_img.className = "img";
-
-    graphicCatalogItems[i].item_name = document.createElement("div");
-    graphicCatalogItems[i].item_name.className = "item_name";
-    graphicCatalogItems[i].item_name.textContent = catalog.Items[i].item_name;
-
-    graphicCatalogItems[i].item_cost = document.createElement("div");
-    graphicCatalogItems[i].item_cost.className = "item_cost";
-    graphicCatalogItems[i].item_cost.textContent = catalog.Items[i].item_cost;
-
-    graphicCatalogItems[i].item_btn = document.createElement("button");
-    graphicCatalogItems[i].item_btn.className = "btn_add";
-    graphicCatalogItems[i].item_btn.textContent = "Добавить";
-
-    graphicCatalogItems[i].add_remove_figures = document.createElement("figure");
-    graphicCatalogItems[i].add_remove_figures.className = "add_remove_figure";
-
-    document.getElementById("items").appendChild(graphicCatalogItems[i]);
-    graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_img);
-    graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_name);
-    graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_cost);
-    graphicCatalogItems[i].appendChild(graphicCatalogItems[i].add_remove_figures);
-    graphicCatalogItems[i].add_remove_figures.appendChild(graphicCatalogItems[i].item_btn);
+.item {
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    height: 190px;
 }
 
-let choose_time_btn = document.createElement("button");
-choose_time_btn.className = "choose_time_btn";
-document.getElementById("items").appendChild(choose_time_btn);
-choose_time_btn.textContent = "Выбрать время";
-
-choose_time_btn.addEventListener("click", () => {
-    tg.sendData(order.generate_data_for_send());
-    console.log("Click to ok btn");
-});
-
-
-for (let i = 1; i < catalog.size + 1; ++i) {
-    graphicCatalogItems[i].item_btn.addEventListener("click", () => {
-        order.order_cost += parseInt(graphicCatalogItems[i].item_cost.textContent);
-
-        graphicCatalogItems[i].item_btn.style.display = "none";
-
-        graphicCatalogItems[i].minus_btn = document.createElement("button");
-        graphicCatalogItems[i].minus_btn.textContent = "-";
-        graphicCatalogItems[i].minus_btn.className = "btn_minus"
-
-        order.order_items[i] = document.createElement("label");
-        order.order_items[i].textContent = "1";
-        order.order_items[i].className = "order_item_label";
-
-        graphicCatalogItems[i].plus_btn = document.createElement("button");
-        graphicCatalogItems[i].plus_btn.textContent = "+";
-        graphicCatalogItems[i].plus_btn.className = "btn_plus"
-
-        graphicCatalogItems[i].add_remove_figures.appendChild(graphicCatalogItems[i].minus_btn);
-        graphicCatalogItems[i].add_remove_figures.appendChild(order.order_items[i]);
-        graphicCatalogItems[i].add_remove_figures.appendChild(graphicCatalogItems[i].plus_btn);
-
-        graphicCatalogItems[i].plus_btn.addEventListener("click", () => {
-            let new_number = +(order.order_items[i].textContent);
-            new_number += 1;
-            order.order_cost += parseInt(graphicCatalogItems[i].item_cost.textContent);
-            order.order_items[i].textContent = new_number + "";
-            console.log(order.generate_data_for_send());
-        });
-
-        graphicCatalogItems[i].minus_btn.addEventListener("click", () => {
-            let new_number = +(order.order_items[i].textContent);
-            if (new_number >= 2) {
-                new_number -= 1;
-                order.order_items[i].textContent = new_number + "";
-            } else {
-                graphicCatalogItems[i].item_btn.style.display = "inline-block";
-                graphicCatalogItems[i].minus_btn.style.display = "none";
-                graphicCatalogItems[i].plus_btn.style.display = "none";
-                order.order_items[i].style.display = "none";
-                order.order_items[i].textContent = "0";
-            }
-            order.order_cost -= parseInt(graphicCatalogItems[i].item_cost.textContent);
-            console.log(order.generate_data_for_send());
-        });
-
-        console.log(order.generate_data_for_send());
-    })
+.img {
+    width: 90px;
 }
 
-// Telegram.WebApp.onEvent("mainButtonClicked", () => {
-//     tg.sendData(order.generate_data_for_send());
-// });
+.add_remove_figure {
+    border-radius: 20px;
+    width: 90px;
+    height: 35px;
+    display: flex;
+    align-items: center;
+    background-color: #ffdaae;
+    margin-top: 8px;
+}
+
+.btn_add {
+    display: inline-block;
+    background-color: gray;
+    padding: 8px 8px 8px 8px;
+    border: none;
+    border-radius: 20px;
+    width: 90px;
+    height: 35px;
+    color: #2d2d2d;
+    background-color: #ffdaae;
+    transition: background-color 0.1s linear;
+    font-size: 13px;
+}
+
+.btn_add:hover, .btn_plus:hover, .btn_minus:hover {
+    background: #ffe4c4;
+}
+
+.btn_plus {
+    border: none;
+    margin-left: 7px;
+    border-radius: 3px 20px 20px 3px;
+    width: 35px;
+    height: 35px;
+    color: #2d2d2d;
+    background-color: #ffdaae;
+    transition: background-color 0.1s linear;
+    font-size: 15px;
+}
+
+.btn_minus {
+    border: none;
+    margin-right: 7px;
+    border-radius: 20px 3px 3px 20px;
+    width: 35px;
+    height: 35px;
+    color: #2d2d2d;
+    background-color: #ffdaae;
+    transition: background-color 0.1s linear;
+    font-size: 15px;
+}
+
+
+.item_name {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 15px;
+    padding-top: 1px;
+}
+
+.item_cost {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 13px;
+    padding-top: 10px;
+    color: #505050;
+}
+
+.order_item_label {
+    width: 24px;
+    font-size: 14px;
+    font-family: 'Open Sans', sans-serif;
+}
+
+.choose_time_btn {
+    width: 120px;
+    height: 40px;
+    position: fixed;
+    margin-left: 135px;
+    margin-right: auto;
+    margin-bottom: auto;
+    bottom: 0px;
+
+    border: none;
+    border-radius: 20px 20px 20px 20px;
+    color: #2d2d2d;
+    background-color: #ffbf74;
+    transition: background-color 0.1s linear;
+    font-size: 13px;
+}
+
+
+.choose_time_btn:hover {
+    background: #ffd099;
+}
