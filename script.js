7246b94e51b498e88c53cba494a307c69e77081b
I2L1A1 <101645426+I2L1A1@users.noreply.github.com>
2024-03-17 19:40:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ document.querySelector(".shopping_cart").classList.add("hidden");
 catalog.addItem("Dish1.png", "Пельмени сибирские", "100");
 catalog.addItem("Dish1.png", "Окрошка настоящая", "200");
 catalog.addItem("Dish1.png", "Щи старорусские с яблоками", "150");
-catalog.addItem("Dish1.png", "Утиная грудка с грушей томлёной", "290");
+catalog.addItem("Dish1.png", "Утиная грудка с грушей томлёной", "290");
 catalog.addItem("Dish1.png", "Биточки из медведя", "175");
 catalog.addItem("Dish1.png", "Бифстекс из кабана", "45");
 catalog.addItem("Dish1.png", "Жаренина изкартофеля", "444");
@@ -189,9 +189,9 @@ choose_time_btn.addEventListener("click", () => {
         back_btn.hide();
     });
 
-    time_slider.classList.remove("hidden");
+    // time_slider.classList.remove("hidden");
     // choose_time_label.classList.remove("hidden");
-    checkout_btn.classList.remove("hidden");
+    // checkout_btn.classList.remove("hidden");
     // cansel_choose_time_btn.classList.remove("hidden");
     document.querySelector(".container").classList.add("bottom_container_margin");
 
