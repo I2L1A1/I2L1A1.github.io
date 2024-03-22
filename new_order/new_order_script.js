08aba8e78552affa286e3854d7a03fd9abea78f2
Ilia Kholopkin <iliagold@icloud.com>
2024-03-22 18:35:42 +0300

    321

--- new_order/new_order_script.js
+++ new_order/new_order_script.js
@@ -312,8 +312,17 @@ get_data_from_server(url_addresses.catalog_url).then((data_from_server) => {
         const textarea = document.querySelector('.order_comment');
 
         textarea.addEventListener('blur', () => {
+            const fakeInput = document.createElement('input');
+            fakeInput.style.position = 'absolute';
+            fakeInput.style.opacity = '0';
+
+            document.body.appendChild(fakeInput);
+
+            console.log(123);
+            fakeInput.focus();
             textarea.blur();
-            document.querySelector(".container").style.backgroundColor = "red";
+
+            document.querySelector(".container").style.backgroundColor = "green";
         });
 
         // let back_btn = document.querySelector(".back_btn");
