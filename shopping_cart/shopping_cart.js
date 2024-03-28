1d8e2d622b2c4bfc07595c0fcf58f0238b34f698
Ilia Kholopkin <iliagold@icloud.com>
2024-03-28 15:20:39 +0300

    margint-bottom: calc(2.2em + 10px)

--- shopping_cart/shopping_cart.js
+++ shopping_cart/shopping_cart.js
@@ -199,16 +199,6 @@ back_btn.addEventListener("click", () => {
     order.push_data_to_cash();
 });
 
-//
-// document.querySelector('.shopping_cart').addEventListener('touchstart', (event) => {
-//     if (!event.target.closest('.order_comment')) {
-//         document.querySelector(".order_comment").tabIndex = -1;
-//         document.querySelector(".shopping_cart").tabIndex = 1;
-//         document.querySelector(".shopping_cart").focus();
-//     } else {
-//     }
-// });
-
 let now_page_position_y = window.scrollY;
 
 document.querySelector('.shopping_cart').addEventListener('touchstart', (event) => {
