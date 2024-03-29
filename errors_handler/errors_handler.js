079df7155191211b78c159969b653b14f04acd24
Ilia Kholopkin <iliagold@icloud.com>
2024-03-29 12:43:16 +0300

    Incorrect url (for test)

--- errors_handler/errors_handler.js
+++ errors_handler/errors_handler.js
@@ -6,6 +6,6 @@ export function show_error(error_number) {
     if (error_number === 500) {
         create_error_label(error_number, "Сервер временно недоступен.");
     } else {
-        create_error_label(123, "Страшная ошибка.");
+        create_error_label("", "Похоже, нет соединения с интернетом.");
     }
 }
