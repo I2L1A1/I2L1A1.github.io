a803c51d92bafabbeabd2a0ebea19c949a7a9e5e
Ilia Kholopkin <iliagold@icloud.com>
2024-04-18 22:49:36 +0300

    321

--- show_orders/show_orders_script.js
+++ show_orders/show_orders_script.js
@@ -69,5 +69,10 @@ get_data_from_server(user_url).then((data_from_server) => {
 });
 
 let header_label = document.querySelector(".header_label");
-header_label.textContent = tg.initDataUnsafe.user.username;
+if (tg.initData.user.username) {
+    console.log(tg.initData.user.username);
+    header_label.textContent = tg.initData.user.username;
+} else {
+    header_label.textContent = "123";
+}
 
