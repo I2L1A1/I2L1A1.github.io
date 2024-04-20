d719a03ed550e3e5d48336cfec726a89cbf000fd
Ilia Kholopkin <iliagold@icloud.com>
2024-04-20 18:59:26 +0300

    delete show_orders

--- categories_script.js
+++ categories_script.js
@@ -1,16 +1,16 @@
-import {animated_page_scroll} from "../tools/animated_page_scroll_tools.js";
-import {categories_url} from "../URL_storage.js";
+import {animated_page_scroll} from "./tools/animated_page_scroll_tools.js";
+import {categories_url} from "./URL_storage.js";
 import {
     get_data_from_server, send_data_to_server
-} from "../tools/networking_tools.js"
+} from "./tools/networking_tools.js"
 import {
     create_element,
     create_image,
     show_element_with_animation,
     hide_element_with_animation,
     push_plus_minus_button_animation, create_error_label
-} from "../tools/graphical_tools.js";
-import {show_error} from "../errors_handler/errors_handler.js";
+} from "./tools/graphical_tools.js";
+import {show_error} from "./errors_handler/errors_handler.js";
 
 let categories = document.querySelector(".categories");
 
@@ -25,7 +25,7 @@ get_data_from_server(categories_url).then((data_from_server) => {
         for (let item in data_from_server) {
             let category_div = create_element("div", "category_div");
             let category_link = create_element("a", "category_link", data_from_server[item]);
-            category_link.href = "../catalog/catalog.html"
+            category_link.href = "catalog/catalog.html"
             category_div.appendChild(category_link);
             categories.appendChild(category_div);
             category_div.addEventListener("click", () => {
